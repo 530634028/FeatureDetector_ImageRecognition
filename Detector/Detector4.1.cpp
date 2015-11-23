ee065d7b809f1755964984d454b9d8f09e721423
frokie <520634028@qq.com>
2015-11-23 12:05:16 +0800

    Change this file for complie.

---
+++ Detector/Detector4.1.cpp
@@ -0,0 +1,17 @@
+/*
+ *Computer vision chapter4
+ *This program is used to compare the  performance between 
+ *various feature detector. For example Laplace, Harris and so 
+ *on.
+ *
+*/
+
+#include "Detector.h"
+
+int main(int argc, char *argv[])
+{
+
+	cout << "hello" << endl;
+	return EXIT_SUCCESS;
+}
+
